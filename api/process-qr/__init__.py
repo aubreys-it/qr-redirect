308164426d80fc5ba377d0b2f6e2e98c86daa4bf
granthopperflash <98110041+granthopperflash@users.noreply.github.com>
2023-05-05 13:47:06 +0000

    Test Commit 2

--- api/process-qr/__init__.py
+++ api/process-qr/__init__.py
@@ -2,6 +2,7 @@ import logging
 import pyodbc
 import os
 import azure.functions as func
+#
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
